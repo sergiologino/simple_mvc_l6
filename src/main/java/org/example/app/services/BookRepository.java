ae454dcde5a246d2ba7c8039a0b0dff0a00955b6
sergiologino <savkin.sergey.s@gmail.com>
2023-10-18 17:50:32 +0300

    Initial commit

---
+++ src/main/java/org/example/app/services/BookRepository.java
@@ -0,0 +1,56 @@
+package org.example.app.services;
+
+import org.apache.log4j.Logger;
+import org.example.web.dto.Book;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class BookRepository<T> implements ProjectRepository<Book>, ApplicationContextAware {
+
+    private final Logger logger = Logger.getLogger(BookRepository.class);
+    private final List<Book> repo = new ArrayList<>();
+    private ApplicationContext context;
+
+    @Override
+    public List<Book> retreiveAll() {
+        return new ArrayList<>(repo);
+    }
+
+    @Override
+    public void store(Book book) {
+        book.setId(book.hashCode());
+        logger.info("store new book: " + book);
+        repo.add(book);
+    }
+
+    @Override
+    public boolean removeItemById(Integer bookIdToRemove) {
+        for (Book book : retreiveAll()) {
+            if (book.getId().equals(bookIdToRemove)) {
+                logger.info("remove book completed: " + book);
+                return repo.remove(book);
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.context = applicationContext;
+    }
+
+    private void defaultInit() {
+        logger.info("default INIT in book repo bean");
+    }
+
+    private void defaultDestroy() {
+        logger.info("default DESTROY in book repo bean");
+    }
+}
+
